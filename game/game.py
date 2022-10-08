b573cbdc043484e2b2f669ceaabbc0173154994d
LucasGobatto <lucas.bisaio@live.com>
2022-10-08 16:11:36 -0300

    feat: add some bridges

--- game/game.py
+++ game/game.py
@@ -5,7 +5,7 @@ from game.map import Map
 class Game():
   def __init__(self):
     pygame.display.init()
-    self.surface = pygame.display.set_mode(size=(WIDTH, HEIGHT))
+    self.surface = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)  
     pygame.display.set_caption("MiniFarm")
 
     self.clock = pygame.time.Clock()
@@ -18,8 +18,10 @@ class Game():
 
   def run(self):
     while True:
+      pressed_keys = pygame.key.get_pressed()
+        
       for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if pressed_keys[pygame.K_ESCAPE] or event.type == pygame.QUIT:
           self.stop()
 
       self.surface.fill(0)
