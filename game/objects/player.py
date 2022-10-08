22cb0ed70c6e7861910fb3399877d41050cb1ded
LucasGobatto <lucas.bisaio@live.com>
2022-10-08 17:45:38 -0300

    feat: improve typing in player class

--- game/objects/player.py
+++ game/objects/player.py
@@ -1,27 +1,31 @@
+from typing import Literal
 import pygame, math
+from pygame.surface import Surface
 from game.utils import import_folder
 from game.constants import CHUNK
 
+DirectionType = Literal["horizontal","vertical"]
+AnimationDirections = Literal["left","right","back","front"]
+
 class Player(pygame.sprite.Sprite):
+  _is_running = False
+  _frame_index = 0
+  _animation_direction: AnimationDirections = "front"
+  _animation_time = 0.1
+  _speed = 3
+  _direction = pygame.math.Vector2()
+  
   def __init__(self, pos, groups, obstacle_sprites):
     super().__init__(groups)
     self.import_image()
-    self.frame_index = 0
-    self.animation_time = 0.1
-    self.is_running = False
-    self.animation_direction = "front"
-    self.image = self.animation["stop"][self.animation_direction][self.frame_index]
+    self.image = self.animation["stop"][self._animation_direction][self._frame_index]
     self.rect = self.image.get_rect(topleft = pos)
     self.hitbox = self.rect.inflate((1 - (CHUNK / 2), 2 - CHUNK * 2))
-
-    self.direction = pygame.math.Vector2()
-    self.speed = 3
-
     self.obstacle_sprites = obstacle_sprites
 
   def import_image(self):
     path = "./assets/player"
-    self.animation: dict[str, dict[str, list[pygame.surface.Surface]]] = {
+    self.animation: dict[str, dict[str, list[Surface]]] = {
       "stop": { "left": [], "right": [], "front": [], "back": []}, 
       "running": { "left": [], "right": [], "front": [], "back": []},
     }
@@ -32,19 +36,19 @@ class Player(pygame.sprite.Sprite):
         self.animation[state][direction] = import_folder(fullpath)
   
   def animate(self):
-    if (self.is_running):
+    if (self._is_running):
       state = "running"
-      self.frame_index += self.animation_time
+      self._frame_index += self._animation_time
     else:
-      self.frame_index += self.animation_time / 2 
+      self._frame_index += self._animation_time / 2 
       state = "stop"
 
-    animation = self.animation[state][self.animation_direction]
+    animation = self.animation[state][self._animation_direction]
 
-    if (self.frame_index >= len(animation)):
-      self.frame_index = 0
+    if (self._frame_index >= len(animation)):
+      self._frame_index = 0
     
-    self.image = animation[int(math.floor(self.frame_index))]
+    self.image = animation[int(math.floor(self._frame_index))]
 
   def update(self):
     self.keyboard_input()
@@ -52,17 +56,17 @@ class Player(pygame.sprite.Sprite):
     self.animate()
 
   def move(self):
-    if (self.direction.x == 0 and self.direction.y == 0):
-      self.is_running = False
+    if (self._direction.x == 0 and self._direction.y == 0):
+      self._is_running = False
     else:
-      self.is_running = True
+      self._is_running = True
 
-    if self.direction.magnitude() != 0:
-      self.direction = self.direction.normalize()
+    if self._direction.magnitude() != 0:
+      self._direction = self._direction.normalize()
 
-    self.hitbox.x += int(self.direction.x * self.speed)
+    self.hitbox.x += int(self._direction.x * self._speed)
     self.collision("horizontal")
-    self.hitbox.y += int(self.direction.y * self.speed)
+    self.hitbox.y += int(self._direction.y * self._speed)
     self.collision("vertical")
     
     if (self.rect):
@@ -75,40 +79,40 @@ class Player(pygame.sprite.Sprite):
     }
 
     events = pygame.key.get_pressed()
-    direction = { "x": 0, "y": 0 }
+    direction: dict[str, int] = { "x": 0, "y": 0 }
    
     if events[pygame.K_UP] or events[pygame.K_w]:
       direction["y"] = movements["decrease"]
-      self.animation_direction = "back"
+      self._animation_direction = "back"
     
     if events[pygame.K_DOWN] or events[pygame.K_s]:
       direction["y"] = movements["increase"]
-      self.animation_direction = "front"
+      self._animation_direction = "front"
 
     if events[pygame.K_LEFT] or events[pygame.K_a]:
       direction["x"] = movements["decrease"]
-      self.animation_direction = "left"
+      self._animation_direction = "left"
 
     if events[pygame.K_RIGHT] or events[pygame.K_d]:
       direction["x"] = movements["increase"]
-      self.animation_direction = "right"
+      self._animation_direction = "right"
 
-    self.direction.x = direction["x"]
-    self.direction.y = direction["y"]
+    self._direction.x = direction["x"]
+    self._direction.y = direction["y"]
 
-  def collision(self, direction):
+  def collision(self, direction: DirectionType):
     if direction == "horizontal":
       for sprite in self.obstacle_sprites:
         if sprite.hitbox.colliderect(self.hitbox):
-          if self.direction.x > 0:
+          if self._direction.x > 0:
             self.hitbox.right = sprite.hitbox.left
-          elif self.direction.x < 0:
+          elif self._direction.x < 0:
             self.hitbox.left = sprite.hitbox.right
 
     if direction == "vertical":
       for sprite in self.obstacle_sprites:
         if sprite.hitbox.colliderect(self.hitbox):
-          if self.direction.y > 0:
+          if self._direction.y > 0:
             self.hitbox.bottom = sprite.hitbox.top
-          elif self.direction.y < 0:
+          elif self._direction.y < 0:
             self.hitbox.top = sprite.hitbox.bottom
